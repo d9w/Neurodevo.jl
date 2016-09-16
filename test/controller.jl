856f7491c492af5e5ab65937ae4680486fe05945
Dennis Wilson <dennis@d9w.xyz>
2016-09-16 20:37:28 +0200

    controller profiling with better correlation

--- test/controller.jl
+++ test/controller.jl
@@ -1,3 +1,4 @@
+using StatsBase
 using Grid
 using DataFrames
 using Gadfly
@@ -5,25 +6,35 @@ using Gadfly
 include("../src/controller.jl")
 include("../src/constants.jl")
 
-n_samples = 100000
+n_samples = 1000
+n_trials = 1000
 
 function profile_graph(cont_func, funcname, outlabels, inlabels, infuns, inform)
   println(funcname)
-  inps = Array{Float64}(n_samples,length(inlabels))
-  outs = Array{Float64}(n_samples,length(outlabels))
-  cors = Array{Float64}(length(inlabels),length(outlabels))
-  for i=1:n_samples
-    inps[i,:] = [infuns[i](0.0) for i in eachindex(infuns)]
-    info = inform(inps[i,:])
-    out = cont_func(info...)
-    if length(out) > 1
-      out = vec(out)
-    end
-    outs[i,:] = out
-  end
-  for i in eachindex(inlabels)
-    for o in eachindex(outlabels)
-      cors[i,o] = abs(corspearman(inps[:,i], outs[:,o]));
+  inps = Array{Float64}(n_samples*n_trials*length(inlabels),length(inlabels))
+  outs = Array{Float64}(n_samples*n_trials*length(inlabels),length(outlabels))
+  cors = Array{Float64}(length(inlabels)*n_trials,length(outlabels))
+  point = 1
+  pc = 1
+  for t=1:n_trials
+    ins = [infuns[i](0.0) for i in eachindex(infuns)]
+    for i=1:length(inlabels)
+      incopy = copy(ins)
+      p1 = copy(point)
+      for s=1:n_samples
+        incopy[i] = infuns[i](0.0)
+        out = cont_func(inform(incopy)...)
+        if length(out) > 1
+          out = vec(out)
+        end
+        inps[point,:] = incopy
+        outs[point,:] = out
+        point+=1
+      end
+      for o=1:length(outlabels)
+        cors[pc,o] = abs(corspearman(inps[p1:p1+n_samples-1,i],outs[p1:p1+n_samples-1,o]))
+      end
+      pc += 1
     end
   end
   cors[isnan(cors)] = 0.0;
@@ -31,8 +42,9 @@ function profile_graph(cont_func, funcname, outlabels, inlabels, infuns, inform)
   ps = Array{Compose.Context}(length(outlabels))
   for o in eachindex(outlabels)
     pv = plot(x=ones(n_samples), y=outs[:,o], Geom.violin, Guide.xlabel(""), Guide.xticks(label=false))
-    pc = plot(x=inlabels, y=cors[:,o], Geom.point, Guide.xlabel(outlabels[o]), Guide.xticks(orientation=:vertical),
-              Guide.yticks(ticks=collect(0.0:0.2:1.0)))
+    pc = plot(x=repmat(inlabels,n_trials), y=cors[:,o], Geom.histogram2d(xbincount=length(inlabels),ybincount=10),
+              Guide.xlabel(outlabels[o]), Guide.xticks(orientation=:vertical),
+              Guide.yticks(ticks=collect(0.0:0.2:1.0)), Guide.colorkey(""))
     if o==1
       append!(pv.guides,[Guide.ylabel("Value")])
       append!(pc.guides,[Guide.ylabel("Correlation")])
@@ -40,10 +52,11 @@ function profile_graph(cont_func, funcname, outlabels, inlabels, infuns, inform)
       append!(pv.guides,[Guide.ylabel("")])
       append!(pc.guides,[Guide.ylabel("")])
     end
+    pc.theme.key_position=:right
     ps[o] = vstack(pv, pc)
   end
   draw(PNG("/home/d9w/Documents/projects/axon-guidance/plots/$funcname.png",
-          (10*length(outlabels))cm, 20cm), hstack(ps...))
+          (15*length(outlabels))cm, 20cm), hstack(ps...))
 end
 
 funcname = "division"
@@ -93,7 +106,7 @@ funcname = "cell_movement"
 outlabels = ["pos$i" for i=1:3]
 inlabels = [["m$i" for i=1:4];["g$i$j" for i=1:4,j=1:3][:];"c";["p$i" for i=1:4];["d$i" for i=1:3]]
 infuns = [[x->10*rand() for i=1:16];x->rand(1:4);[x->rand(1:4) for i=1:4];[x->DIMS[i] for i=1:3]]
-inform = x->(Array{Float64}(x[1:4]),reshape(x[5:16],4,3),Int64(x[17]),Array{Int64}(x[18:21]),
+inform = x->(Array{Float64}(x[1:4]),reshape(Array{Float64}(x[5:16]),4,3),Int64(x[17]),Array{Int64}(x[18:21]),
              Array{Float64}(x[22:24]))
 profile_graph(cell_movement, funcname, outlabels, inlabels, infuns, inform)
 
