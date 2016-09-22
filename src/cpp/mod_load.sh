c6138a44977a5d2a386dd3fa40614692aa57ffa4
Dennis Wilson <dennis@d9w.xyz>
2016-09-22 13:31:48 +0200

    remove old stuff for calmip lightness

--- src/cpp/mod_load.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-module purge
-module load gcc/5.3.0
-module load intelmpi/5.1.2.150
-module list
--- src/cpp/show_grn.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "GRN.h"
-
-int main(int argc, char** argv)
-{
-  GRN grn = GRN(argv[1]);
-  std::cout << grn.toString() << std::endl;
-}
