6dd23dde2b052dcfbe61691d890a336c97d9575d
Dennis Wilson <dennis@d9w.xyz>
2018-03-20 16:47:12 +0100

    yeast and spiral problems with data

--- experiments/datasets.jl
+++ experiments/datasets.jl
@@ -1,15 +1,20 @@
 using RDatasets
 
+function col_normalize(X::Array{Float64})
+    xmin = minimum(X, 1)
+    xmax = maximum(X, 1)
+    for i in 1:size(X, 2)
+        X[:, i] = (X[:, i] .- xmin[i]) ./ (xmax[i] - xmin[i])
+    end
+    X
+end
+
 function get_iris()
     iris = dataset("datasets", "iris")
 
-    iris_data = Array{Float64}([iris[:SepalLength] iris[:SepalWidth] iris[:PetalWidth] iris[:PetalLength]]);
-    xmin = minimum(iris_data, 1)
-    xmax = maximum(iris_data, 1)
-    X = Array{Float64}(size(iris_data, 2), size(iris_data, 1))
-    for i in 1:size(iris_data, 2)
-        X[i, :] = (iris_data[:, i] .- xmin[i]) ./ (xmax[i] - xmin[i])
-    end
+    iris_data = Array{Float64}(hcat(iris[:SepalLength], iris[:SepalWidth],
+                                    iris[:PetalWidth], iris[:PetalLength]));
+    X = col_normalize(iris_data)'
 
     sps = unique(iris[:Species])
     iris[:label] = indexin(iris[:Species], sps)
@@ -17,8 +22,34 @@ function get_iris()
     X, Y
 end
 
+function get_spirals()
+    spirals = readtable("data/spiral.txt", header=false, separator=' ',
+                        names=[:x, :y, :label])
+    X = col_normalize(Array{Float64}(hcat(spirals[:x], spirals[:y])))'
+    Y = Array{Int64}(spirals[:label])
+    X, Y
+end
+
+function get_yeast()
+    yeast = readtable("data/yeast.data", header=false, separator=' ',
+                      names=[:seq, :mcg, :gvh, :alm, :mit, :erl, :pox, :vac,
+                             :nuc, :local])
+    yeast_data = Array{Float64}(hcat(
+        map(i->yeast[i],[:mcg, :gvh, :alm, :mit, :erl, :pox, :vac, :nuc])...))
+    X = col_normalize(yeast_data)'
+    locs = unique(yeast[:local])
+    yeast[:label] = indexin(yeast[:local], locs)
+    Y = Array{Int64}(yeast[:label])
+    X, Y
+end
+
 function get_data(problem::String)
     if problem == "iris"
         return get_iris()
+    elseif problem == "spirals"
+        return get_spirals()
+    elseif problem == "yeast"
+        return get_yeast()
     end
+    error("Problem not found")
 end
