528c79cfa7c4ddf12ba468fb84e7db5c728e81f0
Dennis Wilson <dennis@d9w.xyz>
2018-12-19 14:57:51 +0100

    individual training plot

---
+++ plot_individual.jl
@@ -0,0 +1,90 @@
+using JSON
+include("plot.jl")
+include("evo/data.jl")
+
+function get_individual(filename::String)
+    ind_dict = JSON.parse(open(filename, "r"))
+    genes = Array{Array{Float64}}(ind_dict["genes"])
+    fitness = Array{Float64}(ind_dict["fitness"])
+    NeurodevoInd(genes, fitness)
+end
+
+function classify_performance(m::Model, X::Array{Float64}, Y::Array{Int64},
+                              dataset::String, seed::Int64)
+    total_time = 0
+    total_memory = 0
+    df = DataFrame(seed=Int[], dataset=String[], epoch=Int[], index=Int[], label=Int[],
+                   guess=Int[], certainty=Float64[], correct=Int[])
+    for epoch in 1:m.cfg["n_epochs"]
+        labels = zeros(Int64, length(Y))
+        correct = 0
+        for i in eachindex(Y)
+            outputs, t, bytes, gctime, memallocs = @timed Neurodevo.step!(m, X[:, i])
+            labels[i] = argmax(outputs)
+            if labels[i] == Y[i]
+                correct += 1
+                Neurodevo.reward!(m, [1.0])
+            end
+            push!(df, [seed, dataset, epoch, i, Y[i], labels[i],
+                       outputs[labels[i]], correct])
+        end
+    end
+    df
+end
+
+function get_performance(ind::NeurodevoInd)
+    cfg = cgp_cfg(Config("cfg/evo.yaml"), ind.genes[1])
+    c = cgp_controller(cfg, ind.genes[2:end])
+    m = Model(cfg, c)
+
+    X, Y = ind.func(ind.seed)
+    nin = size(X, 1)
+    nout = length(unique(Y))
+    nsample = length(Y)
+    layered_init!(m, nin, nout; nhidden=nin, nreward=1)
+    if cfg["init_method"] == 0
+        random_init!(m, nin, nout; nreward=1, nhidden=cfg["nhidden"])
+    else
+        layered_init!(m, nin, nout; nreward=1, nhidden=cfg["nhidden"])
+    end
+    develop!(m)
+    df = classify_performance(m, X, Y, String(split(repr(ind.func), "_")[2]),
+                              ind.seed)
+    df[:step] = df[:index] + (df[:epoch] .- 1) * nsample
+    df[:accuracy] = df[:correct] ./ df[:index]
+    df
+end
+
+function plot_performance(res::DataFrame; title="Training accuracy",
+                          filename="training.pdf")
+    plt = plot(res, x=:step, y=:accuracy, color=:epoch,
+               Geom.smooth,
+               Guide.xlabel("Step"),
+               Guide.ylabel("Accuracy"),
+               Scale.color_discrete,
+               Guide.title(title))
+    draw(PDF(filename, 8inch, 6inch), plt)
+end
+
+function plot_all_data(ind::NeurodevoInd, id::String)
+    res = get_performance(ind)
+    plot_performance(res; title="Iris training accuracy",
+                     filename=string(id, "_iris_training.pdf"))
+    ind.func = get_diabetes
+    res = get_performance(ind)
+    plot_performance(res; title="Diabetes training accuracy",
+                     filename=string(id, "_diabetes_training.pdf"))
+    ind.func = get_glass
+    res = get_performance(ind)
+    plot_performance(res; title="Glass training accuracy",
+                     filename=string(id, "_glass_training.pdf"))
+end
+
+function plot_all()
+    ind = get_individual("gens/1/1000/0100.dna")
+    plot_all_data(ind, "1_1000")
+    ind = get_individual("gens/1/2000/0100.dna")
+    plot_all_data(ind, "1_2000")
+    ind = get_individual("gens/89462/1000/0100.dna")
+    plot_all_data(ind, "89462")
+end
