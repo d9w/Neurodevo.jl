1de675972434559fc8ec94233b7c170058ae169a
Dennis Wilson <dennis@d9w.xyz>
2018-12-20 01:24:22 +0100

    cgp genes from 2nd entry

--- evo/data.jl
+++ evo/data.jl
@@ -32,7 +32,7 @@ end
 
 function data_evaluation(ind::NeurodevoInd)
     cfg = cgp_cfg(Config("cfg/evo.yaml"), ind.genes[1])
-    c = cgp_controller(cfg, ind.genes)
+    c = cgp_controller(cfg, ind.genes[2:end])
     m = Model(cfg, c)
 
     X, Y = ind.func(ind.seed)
